24b9a3ed40fab97a90d5ceba0ad44510e6b8a504
Meral Maradia <39996589+hi-meral@users.noreply.github.com>
2022-05-17 20:23:27 +0530

    Update 3_odd_even_numbers.py
    sort solution using range

--- data_structures/2_Arrays/Solution/3_odd_even_numbers.py
+++ data_structures/2_Arrays/Solution/3_odd_even_numbers.py
@@ -7,3 +7,12 @@ for i in range(1, max):
         odd_numbers.append(i)
 
 print("Odd numbers: ", odd_numbers)
+
+
+
+# Sir, can't we simply do the following
+'''
+x = int(input("Please add max number : "))
+odd_num = list(range(1, x+1, 2))
+print(odd_num)
+'''
